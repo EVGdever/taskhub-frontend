0f0d89c70455a80dcbc089c7d6f2a9d8628e8e18
youbobyyoubo <wheelhmt@gmail.com>
2025-11-17 16:08:11 +0700

    Создал окно регистрации

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {RegisterPage} from "./pages/register/register"
 
 function App() {
   return (
     <div className="App">
-      taskHub
+        <RegisterPage/>
     </div>
   );
 }
 
 export default App;
+
+
